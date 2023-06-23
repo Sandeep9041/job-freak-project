2fe6a6e9ea53367bf5c35f744445a12ada9b3c9f
Sandeep9041 <sandeepkaur.sk.9041@gmail.com>
2023-06-23 16:42:50 +0530

    new-changes

--- src/Components/Contact/contact.js
+++ src/Components/Contact/contact.js
@@ -8,18 +8,18 @@ const Contact = () =>
     <div className="w-2/3 pl-16">
     <h1 className="text-5xl text-slate-800 pt-20 font-serif">Contact Us!</h1>
   <form className="pt-16 flex flex-col">
-    <label htmlFor="fname" className="text-sm text-slate-800 font-sans pb-0.5">First Name</label>
+    <label htmlFor="fname" className="text-sm text-slate-800 font-serif pb-0.5">First Name</label>
     <input type="text" className="rounded text-sm font-sans border border-slate-300 p-2 mb-4" id="fname" name="firstname" placeholder="Your name.."/>
 
-    <label htmlFor="lname" className="text-sm text-slate-800 font-sans  pb-0.5">Last Name</label>
+    <label htmlFor="lname" className="text-sm text-slate-800 font-serif  pb-0.5">Last Name</label>
     <input type="text" className="rounded text-sm font-sans border border-slate-300 p-2 mb-4" id="lname" name="lastname" placeholder="Your last name.."/>
 
     
 
-    <label htmlFor="subject" className="text-sm text-slate-800 font-sans  pb-0.5">Message</label>
+    <label htmlFor="subject" className="text-sm text-slate-800 font-serif  pb-0.5">Message</label>
     <textarea id="subject" className="rounded text-sm font-sans border border-slate-300 p-2 mb-4" name="subject" rows="5" cols="" placeholder="Write something.." style={{height:"200px"}}></textarea>
 
-    <input type="submit" className="font-sans text-sm rounded bg-slate-950 w-24 h-10 text-white" value="Submit"/>
+    <input type="submit" className="font-serif text-sm rounded bg-slate-950 w-24 h-10 text-white" value="Submit"/>
   </form>
     </div>
     </div>
